c4fcd0cb4d942b517443451adf87f883b8b79b56
Henrique Rocha <rochahenrique18@gmail.com>
2026-02-07 18:10:25 -0300

    wip

---
+++ app/Actions/Tenant/Vehicle/SearchVehicleAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Actions\Tenant\Vehicle;
+
+use App\Dto\SearchDto;
+use App\Models\Tenant\Vehicle;
+use App\Traits\QueryBuilderTrait;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class SearchVehicleAction
+{
+    use QueryBuilderTrait;
+
+    public function __invoke(SearchDto $searchDto): LengthAwarePaginator
+    {
+        $query = Vehicle::query()->with('client:id,name');
+
+        return $this->applySearchAndFilters(
+            query: $query,
+            searchDTO: $searchDto,
+            searchableColumns: ['license_plate', 'brand', 'model', 'vin'],
+        );
+    }
+}
