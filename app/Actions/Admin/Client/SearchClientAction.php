ba1e0c28054da89ab8571bf7787816a051a98d34
Henrique Rocha <37241677+henriquerocha2004@users.noreply.github.com>
2026-04-20 09:49:14 -0300

    adicionando logs rotina de clients

--- app/Actions/Admin/Client/SearchClientAction.php
+++ app/Actions/Admin/Client/SearchClientAction.php
@@ -6,6 +6,8 @@ use App\Dto\SearchDto;
 use App\Models\Admin\Client;
 use App\Traits\QueryBuilderTrait;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SearchClientAction
 {
@@ -13,8 +15,19 @@ class SearchClientAction
 
     public function __invoke(SearchDto $searchDto): LengthAwarePaginator
     {
+        $query = Client::query();
+
+        Log::info('[AdminClients] SearchClientAction query', [
+            'model_connection' => $query->getModel()->getConnectionName(),
+            'table'            => $query->getModel()->getTable(),
+            'raw_sql_preview'  => $query->toSql(),
+            'total_in_table'   => DB::connection($query->getModel()->getConnectionName())
+                ->table($query->getModel()->getTable())
+                ->count(),
+        ]);
+
         return $this->applySearchAndFilters(
-            query: Client::query(),
+            query: $query,
             searchDTO: $searchDto,
             searchableColumns: ['name', 'document', 'email'],
         );
